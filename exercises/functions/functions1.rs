0a63ae1256df019a8b27dd2b873eb760d65339a3
lemonsuqing <1428828189@qq.com>
2023-10-10 00:57:51 +0000

    10.10-1

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,7 +3,11 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+fn call_me(){
+    println!("114514");
+}
 
 fn main() {
     call_me();
