420fd4c18acffdf29981026e15ef3cad76242845
lemonsuqing <1428828189@qq.com>
2023-10-12 07:44:11 +0000

    38%

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -43,6 +47,11 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        self.color = (255,0,255);
+        self.position = Point{x:10, y:15};
+        self.quit = true;
+        //self.message = String::from("Hello world!");
+        self.message = "Hello world!".to_string();
     }
 }
 
