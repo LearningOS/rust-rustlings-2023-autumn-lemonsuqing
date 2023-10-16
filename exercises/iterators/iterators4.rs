541f78120550ad4d166fecf906eb96363fbfaf18
lemonsuqing <1428828189@qq.com>
2023-10-16 10:44:05 +0800

    75%

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,22 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let mut number: u64 = num;
+    let mut sum: u64 = 1;
+    if number == 0{
+        return 1;
+    }else{
+        for _ in 0..number{
+            if number != 0{
+                sum *= number;
+                number -= 1;
+            }else{
+                return sum;
+            }
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
